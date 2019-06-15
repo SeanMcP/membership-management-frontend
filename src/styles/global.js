3fb6a83b88eee62ba5ee04de6ad73d41d10ccffc
Sean McPherson <seanmcp25@gmail.com>
2019-06-15 14:58:24 -0400

    Use theme variables

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,6 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
     body {
+        background: ${p => p.theme.background};
+        color: ${p => p.theme.text};
         margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
             'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
