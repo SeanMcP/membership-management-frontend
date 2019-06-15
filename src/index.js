8a35a6c2896cdfa466171b84d71c4ce962ddc448
Sean McPherson <seanmcp25@gmail.com>
2019-06-15 14:45:26 -0400

    Add react-axe, styled-components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker'
+
+if (process.env.NODE_ENV === 'development') {
+    const axe = require('react-axe')
+    axe(React, ReactDOM, 1000)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
