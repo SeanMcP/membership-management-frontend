35e162064e49cd5434531faee470113ee3925a2d
Sean McPherson <seanmcp25@gmail.com>
2019-06-17 21:15:56 -0400

    Setup redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import { AppContextProvider } from 'context/app/AppContext'
 import StyleProvider from 'styles/StyleProvider'
+import store from 'store/index'
 import Router from 'routing/Router'
 import * as S from './App.styled'
 
 function App() {
     return (
         <StyleProvider>
-            <AppContextProvider>
-                <S.Container>
-                    <Router />
-                </S.Container>
-            </AppContextProvider>
+            <Provider store={store}>
+                <AppContextProvider>
+                    <S.Container>
+                        <Router />
+                    </S.Container>
+                </AppContextProvider>
+            </Provider>
         </StyleProvider>
     )
 }
