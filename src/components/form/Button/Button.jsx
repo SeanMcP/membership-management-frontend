5f255710d47670edabc489e6c51f7180452247c5
Sean McPherson <seanmcp25@gmail.com>
2019-06-19 06:42:04 -0400

    Add Button

---
+++ src/components/form/Button/Button.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export default styled.button`
+  background: ${p => p.theme.primary};
+  border: 0;
+  border-radius: ${p => p.theme.borderRadius};
+  color: ${p => p.theme.text_alt};
+  cursor: pointer;
+  font-family: inherit;
+  font-size: inherit;
+  padding: 0.5rem 0.75rem;
+
+  &:focus {
+    box-shadow: 0 0 4px 2px ${p => p.theme.accent};
+    outline: 4px solid transparent;
+  }
+`
