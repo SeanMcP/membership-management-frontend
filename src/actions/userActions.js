31665076eacd00c6e840624834fa5e48aaf40b83
Sean McPherson <seanmcp25@gmail.com>
2019-06-20 06:44:36 -0400

    Add login action creator

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,5 +1,25 @@
+import axios from 'axios'
+
 export const USER_ACTIONS = {
-  SET_USER: 'SET_USER'
+  SET_USER: 'SET_USER',
+  LOGIN: 'LOGIN',
+  REGISTER: 'REGISTER'
+}
+
+export const login = (email, password) => {
+  const url = 'http://localhost:3001/users/login'
+  return async dispatch => {
+    const user = await axios.post(url, {
+      email,
+      password
+    })
+    dispatch({
+      type: USER_ACTIONS.LOGIN,
+      payload: {
+        user
+      }
+    })
+  }
 }
 
 export const setUser = name => ({
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,18 @@
 import { USER_ACTIONS } from 'actions/userActions'
 
 export const userState = {
-  name: 'Neil Amin'
+  name: 'Neil Amin',
+  user: {}
 }
 
 export default function(state = userState, { type, payload }) {
   switch (type) {
+    case USER_ACTIONS.LOGIN: {
+      return {
+        ...state,
+        user: payload.user
+      }
+    }
     case USER_ACTIONS.SET_USER: {
       return {
         ...state,
