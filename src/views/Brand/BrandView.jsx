b625075b21221521b2e15cf718af18a2938e63f4
Sean McPherson <seanmcp25@gmail.com>
2019-06-24 21:51:14 -0400

    Style brand view

--- src/views/Brand/BrandView.jsx
+++ src/views/Brand/BrandView.jsx
@@ -3,30 +3,40 @@ import React from 'react'
 import Content from 'components/layout/Content/Content'
 import ViewWrapper from 'components/layout/ViewWrapper/ViewWrapper'
 import { useBrandContext } from 'context/brand/BrandContext'
+import ColorInput from 'components/brand/ColorInput/ColorInput'
+import * as S from './BrandView.styled'
 
 const BrandView = props => {
   const [state, dispatch] = useBrandContext()
   const fields = []
   for (const field in state) {
     fields.push(
-      <label key={field}>
-        <b>{field}</b>
-        <input
-          type="color"
-          value={state[field]}
-          onChange={e => {
-            dispatch({
-              type: 'UPDATE_BRAND',
-              payload: { key: field, value: e.target.value }
-            })
-          }}
-        />
-      </label>
+      <ColorInput
+        field={field}
+        key={field}
+        onChange={e => {
+          dispatch({
+            type: 'UPDATE_BRAND',
+            payload: { key: field, value: e.target.value }
+          })
+        }}
+        value={state[field]}
+      />
     )
   }
   return (
     <ViewWrapper title="Brand">
-      <Content>{fields}</Content>
+      <Content>
+        <h1>Brand</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+          assumenda aspernatur laborum iusto ipsam voluptas quos a, tempora
+          nulla nemo in mollitia fugit, totam vero, doloribus quo velit
+          perspiciatis non.
+        </p>
+        <h2>Variables</h2>
+        <S.Grid>{fields}</S.Grid>
+      </Content>
     </ViewWrapper>
   )
 }
